33aca3b94e2e9d1415ac586c2784ae0ea664b16a
steven <siter@126.com>
2019-06-11 14:28:44 +0800

    20190611

--- src/main/java/org/asyou/sequoia/query/QuerySymbol.java
+++
@@ -1,5 +0,0 @@
-package org.asyou.sequoia.query;
-
-public class QuerySymbol {
-    public final static char dollar = '$';
-}
--- src/test/java/proxy/dynamic/ITalk.java
+++
@@ -1,9 +0,0 @@
-package proxy.dynamic;
-
-/**
- * Created by steven on 2017/7/19.
- */
-public interface ITalk {
-    void talk() throws Throwable;
-    void sing() throws Throwable;
-}
